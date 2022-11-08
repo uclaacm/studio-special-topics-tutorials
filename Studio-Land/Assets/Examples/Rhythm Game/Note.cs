4cdd67081061a9becad187c24cf18ca4d6fa8b50
Austin Law <austinlaw8@gmail.com>
2022-11-07 16:04:27 -0800

    Adds docs for Note.cs

--- Studio-Land/Assets/Examples/Rhythm Game/Note.cs
+++ Studio-Land/Assets/Examples/Rhythm Game/Note.cs
@@ -2,36 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>Note</c> that encapsulates a note.
+/// Updates its position to emulate falling, and maintains when it is supposed to be pressed.
+/// Note that the beats are given in seconds, while the leeway is given in beats.
+/// </summary>
 public class Note : MonoBehaviour
 {
+    /* Some hard coded constants, such as start position and target end position */
     public static float FALL_LINE = -2f;
     public static Vector3 START_POS = new Vector3(0f,5f,0f);
     public static Vector3 TARGET = new Vector3(0f,FALL_LINE,0f);
-    public static float DEFAULT_LEEWAY = .125f;
 
+    /* Constants that determine drop speed and time */
     public static float fallTime = 2f;
     public static float fallSpeed = Vector3.Distance(TARGET, START_POS) / fallTime;
 
-    public Player player;
-    public BeatController controller;
+    /* The default amount of leeway we allow the player, in beats */
+    public static float DEFAULT_LEEWAY = .125f;
 
-    public float lifetime;
+    /* External objects */
+    private Player player;
+    private BeatController controller;
 
     /* Times when the note starts and ends, in seconds */
     private float beatStart;
     private float beatEnd;
 
-    public float getBeatStart() { return beatStart; }
-    public float getBeatEnd() { return beatEnd; }
-    public bool isHold() { return (beatEnd - beatStart) > (2 * DEFAULT_LEEWAY); }
+    /* How long this note has been alive for, used for lerping */
+    private float lifetime;
 
-    public void SetInitialState (float start, float end, float time) {
+    /// <summary>
+    /// Method <c>SetInitialState</c> sets start and end times for beats, and the current lifetime of the note.
+    ///     Lifetime has to be set because Update(s) aren't completely synced with the beat of the music.
+    ///     That is to say, the controller usually spawns a note ~0.02 seconds after it should spawn.
+    /// Also sets the y-scale such that the note's size is exactly when the user can hit it.
+    /// </summary>
+    public void SetInitialState (float start, float end, float time)
+    {
         beatStart = start;
         beatEnd = end;
         lifetime = time;
         this.transform.localScale = new Vector3(1, (beatEnd - beatStart) * controller.secondsPerBeat * fallSpeed, 1);
     }
 
+    // Lerps and checks destruction
     void Update()
     {
         lifetime += Time.deltaTime;
@@ -39,11 +54,10 @@ public class Note : MonoBehaviour
         lerpPosition();
 
         if (this.transform.position.y + this.transform.localScale.y / 2f < FALL_LINE)
-        {
             DestroyNote();
-        }
     }
 
+    // Destroys note and resets player combo
     private void DestroyNote()
     {
         if (player) player.resetPlayerCombo();
@@ -51,13 +65,21 @@ public class Note : MonoBehaviour
         GameObject.Destroy(this.gameObject);
     }
 
+    // Lerps the note
     private void lerpPosition()
     {
-        float nextX, nextY, nextZ;
-        
-        nextX = Mathf.LerpUnclamped(START_POS.x, TARGET.x, lifetime / fallTime);
-        nextY = Mathf.LerpUnclamped(START_POS.y, TARGET.y, lifetime / fallTime);
-        nextZ = Mathf.LerpUnclamped(START_POS.z, TARGET.z, lifetime / fallTime);
-        this.transform.position = new Vector3(nextX, nextY, nextZ);
+        // We use `LerpUnclamped` so that the note can go just past the target without stopping
+        // and then get destroyed
+        this.transform.position = new Vector3(
+            Mathf.LerpUnclamped(START_POS.x, TARGET.x, lifetime / fallTime),
+            Mathf.LerpUnclamped(START_POS.y, TARGET.y, lifetime / fallTime),
+            Mathf.LerpUnclamped(START_POS.z, TARGET.z, lifetime / fallTime)
+        );
     }
+
+    /* Getters and Setters */ 
+    public float getBeatStart() { return beatStart; }
+    public float getBeatEnd() { return beatEnd; }
+    private void SetPlayer(Player player) { this.player = player; }
+    private void SetController(BeatController controller) { this.controller = controller; }
 }
